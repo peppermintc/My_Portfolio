e91dc0785b6e51b523ba996a0a8b24772e61bf95
peppermintc <haminjp@naver.comm>
2020-04-12 21:13:00 +0900

    Updates

---
+++ precache-manifest.f093b800e1d526a9c612805798c1fbc7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917fa82b7d598494b18bef58d35ec373",
+    "url": "/my_portfoilio/index.html"
+  },
+  {
+    "revision": "cd8d5fdb1fbc6521ca5b",
+    "url": "/my_portfoilio/static/css/main.f7bc5d5b.chunk.css"
+  },
+  {
+    "revision": "757239ac1eac77c51ad6",
+    "url": "/my_portfoilio/static/js/2.2ca84537.chunk.js"
+  },
+  {
+    "revision": "cd8d5fdb1fbc6521ca5b",
+    "url": "/my_portfoilio/static/js/main.e692f6d1.chunk.js"
+  },
+  {
+    "revision": "f0b07d28db1076c3d236",
+    "url": "/my_portfoilio/static/js/runtime-main.131c358f.js"
+  },
+  {
+    "revision": "775dfe7f630b37076c29d0e16e21ffcf",
+    "url": "/my_portfoilio/static/media/email.775dfe7f.png"
+  },
+  {
+    "revision": "9c5b859f19069f1023be574ba63565be",
+    "url": "/my_portfoilio/static/media/js2.9c5b859f.png"
+  },
+  {
+    "revision": "78f8d7bf8b1ba906c3b7227385610e53",
+    "url": "/my_portfoilio/static/media/noteicon.78f8d7bf.png"
+  },
+  {
+    "revision": "e56e37dea453afc79e0a029d0c615488",
+    "url": "/my_portfoilio/static/media/smalljsicon.e56e37de.png"
+  },
+  {
+    "revision": "da006e69fd12e561861013c08982e96e",
+    "url": "/my_portfoilio/static/media/smalljsonicon.da006e69.png"
+  }
+]);
